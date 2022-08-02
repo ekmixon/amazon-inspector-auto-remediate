99d130c7158ef280fa1581eeea54c3809bdd8edc
Sourcery AI <>
2022-08-02 19:07:26 +0000

    'Refactored by Sourcery'

--- lambda-auto-remediate.py
+++ lambda-auto-remediate.py
@@ -11,10 +11,9 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # quick function to handle datetime serialization problems
-enco = lambda obj: (
-    obj.isoformat()
-    if isinstance(obj, datetime.datetime)
-    or isinstance(obj, datetime.date)
+enco = (
+    lambda obj: obj.isoformat()
+    if isinstance(obj, (datetime.datetime, datetime.date))
     else None
 )
 
@@ -25,20 +24,23 @@ def lambda_handler(event, context):
 
     # extract the message that Inspector sent via SNS
     message = event['Records'][0]['Sns']['Message']
-    logger.debug('Event from SNS: ' + message)
-    
+    logger.debug(f'Event from SNS: {message}')
+
     # get inspector notification type
     notificationType = json.loads(message)['event']
-    logger.info('Inspector SNS message type: ' + notificationType)
+    logger.info(f'Inspector SNS message type: {notificationType}')
 
     # skip everything except report_finding notifications
     if notificationType != "FINDING_REPORTED":
-        logger.info('Skipping notification that is not a new finding: ' + notificationType)
+        logger.info(
+            f'Skipping notification that is not a new finding: {notificationType}'
+        )
+
         return 1
-    
+
     # extract finding ARN
     findingArn = json.loads(message)['finding']
-    logger.info('Finding ARN: ' + findingArn)
+    logger.info(f'Finding ARN: {findingArn}')
 
     # get finding and extract detail
     response = inspector.describe_findings(findingArns = [ findingArn ], locale='EN_US')
@@ -47,51 +49,55 @@ def lambda_handler(event, context):
     finding = response['findings'][0]
     logger.debug('Raw finding:')
     logger.debug(finding)
-   
+
 
     # skip uninteresting findings
     title = finding['title']
-    logger.debug('Finding title: ' + title)
+    logger.debug(f'Finding title: {title}')
 
     if title == "Unsupported Operating System or Version":
-        logger.info('Skipping finding: ' + title)
+        logger.info(f'Skipping finding: {title}')
         return 1
-        
+
     if title == "No potential security issues found":
-        logger.info('Skipping finding: ' + title)
+        logger.info(f'Skipping finding: {title}')
         return 1
-    
+
     service = finding['service']
-    logger.debug('Service: ' + service)
+    logger.debug(f'Service: {service}')
     if service != "Inspector":
-        logger.info('Skipping finding from service: ' + service)
+        logger.info(f'Skipping finding from service: {service}')
         return 1
-    
-    cveId = ""
-    for attribute in finding['attributes']:
-        if attribute['key'] == "CVE_ID":
-            cveId = attribute['value']
-            break
-    logger.info('CVE ID: ' + cveId)
-    
+
+    cveId = next(
+        (
+            attribute['value']
+            for attribute in finding['attributes']
+            if attribute['key'] == "CVE_ID"
+        ),
+        "",
+    )
+
+    logger.info(f'CVE ID: {cveId}')
+
     if cveId == "":
         logger.info('Skipping non-CVE finding (could not find CVE ID)')
         return 1
-    
+
     assetType = finding['assetType']
-    logger.debug('Asset type: ' + assetType)
+    logger.debug(f'Asset type: {assetType}')
     if assetType != "ec2-instance":
-        logger.info('Skipping non-EC2-instance asset type: ' + assetType)
+        logger.info(f'Skipping non-EC2-instance asset type: {assetType}')
         return 1
-  
+
     instanceId = finding['assetAttributes']['agentId']
-    logger.info('Instance ID: ' + instanceId)
+    logger.info(f'Instance ID: {instanceId}')
     if not instanceId.startswith("i-"):
-        logger.info('Invalid instance ID: ' + instanceId)
+        logger.info(f'Invalid instance ID: {instanceId}')
         return 1
-        
+
     # if we got here, we have a valid CVE type finding for an EC2 instance with a well-formed instance ID
-    
+
     # query SSM for information about this instance
     filterList = [ { 'key': 'InstanceIds', 'valueSet': [ instanceId ] } ]
     response = ssm.describe_instance_information( InstanceInformationFilterList = filterList, MaxResults = 50 )
@@ -101,29 +107,29 @@ def lambda_handler(event, context):
     logger.debug('Instance information:')
     logger.debug(instanceInfo)
     pingStatus = instanceInfo['PingStatus']
-    logger.info('SSM status of instance: ' + pingStatus)
+    logger.info(f'SSM status of instance: {pingStatus}')
     lastPingTime = instanceInfo['LastPingDateTime']
     logger.debug('SSM last contact:')
     logger.debug(lastPingTime)
     agentVersion = instanceInfo['AgentVersion']
-    logger.debug('SSM agent version: ' + agentVersion)
+    logger.debug(f'SSM agent version: {agentVersion}')
     platformType = instanceInfo['PlatformType']
-    logger.info('OS type: ' + platformType)
+    logger.info(f'OS type: {platformType}')
     osName = instanceInfo['PlatformName']
-    logger.info('OS name: ' + osName)
+    logger.info(f'OS name: {osName}')
     osVersion = instanceInfo['PlatformVersion']
-    logger.info('OS version: ' + osVersion)
-    
+    logger.info(f'OS version: {osVersion}')
+
     # Terminate if SSM agent is offline
     if pingStatus != 'Online':
-        logger.info('SSM agent for this instance is not online: ' + pingStatus)
+        logger.info(f'SSM agent for this instance is not online: {pingStatus}')
         return 1
-    
+
     # This script only supports remediation on Linux
     if platformType != "Linux":
-        logger.info('Skipping non-Linux platform: ' + platformType)
+        logger.info(f'Skipping non-Linux platform: {platformType}')
         return 1
-        
+
     # Look up the correct command to update this Linux distro
     # to-do: patch only CVEs, or patch only the specific CVE
     if osName == 'Ubuntu':
@@ -131,10 +137,10 @@ def lambda_handler(event, context):
     elif osName == 'Amazon Linux AMI':
         commandLine = "yum update -q -y; yum upgrade -y"
     else:
-        logger.info('Unsupported Linux distribution: ' + osName)
+        logger.info(f'Unsupported Linux distribution: {osName}')
         return 1
-    logger.info('Command line to execute: ' + commandLine)
-    
+    logger.info(f'Command line to execute: {commandLine}')
+
     # now we SSM run-command
     response = ssm.send_command(
         InstanceIds = [ instanceId ],
@@ -142,6 +148,6 @@ def lambda_handler(event, context):
         Comment = 'Lambda function performing Inspector CVE finding auto-remediation',
         Parameters = { 'commands': [ commandLine ] }
         )
-    
+
     logger.info('SSM send-command response:')
     logger.info(response)
